f525ef6d2c9a44bbc7683ab5f3fde07c356f2f0e
Pritam Bhatkar <pb457@njit.edu>
2018-10-06 22:44:50 -0400

    Task:Added columns to CSV file and changed background color of table

--- public/index.php
+++ public/index.php
@@ -30,7 +30,7 @@ class html
         $table .='<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>';
         $table .='<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>';
         $table.='<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>';
-        $table = '<table border="1">';
+        $table = '<table border="1" bgcolor= #deb887>';
 
         $table .= row::tableRow($records);
         $table .= '</table>';
@@ -77,7 +77,7 @@ class row{
 
             foreach ($value as $key2 => $value2) {
                 if($flag){
-                    $table .= "<th>".htmlspecialchars($value2)."</th>";
+                    $table .= "<th align='left'>".htmlspecialchars($value2)."</th>";
 
                 }else{
                     $table .= '<td>' . htmlspecialchars($value2) . '</td>';
